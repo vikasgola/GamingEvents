721cfd3cb70a30e0e55030a6dc5427dffa0a460f
Vikas Gola <vikasgola2015@gmail.com>
2018-02-13 06:01:55 +0530

    toolbar added

--- app/src/main/java/io/github/vikasgola/gamingevent/MainActivity.java
+++ app/src/main/java/io/github/vikasgola/gamingevent/MainActivity.java
@@ -1,13 +1,19 @@
 package io.github.vikasgola.gamingevent;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
     }
 }
